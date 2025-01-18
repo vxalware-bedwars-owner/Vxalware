d6ed96050d5ca4172be77b7caad79bcd638f1582
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-01-18 15:20:25 +0800

    Update Vxware-Bedwars.lua

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -1,4 +1,4 @@
 getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
-loadstring(game:HttpGet('https://pastebin.com/raw/08HHpB92'))()
-loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
+loadstring(game:HttpGet('https://pastebin.com/raw/08HHpB92'))() -- Vxalware Extension
+loadstring(game:HttpGet("https://dpaste.com/CMGU6RSJW.txt",true))() -- AlSploit
