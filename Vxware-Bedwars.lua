ee67e6464f45d8564cb346ccb0c1f486ff0a1af7
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-02-01 22:01:04 +0800

    Update Vxware-Bedwars.lua

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -4,8 +4,6 @@ local UserInputService = game:GetService("UserInputService")
 local function detectDevice()
     if UserInputService.TouchEnabled then
         -- The player is on a mobile device
-        loadstring(game:HttpGet('https://pastebin.com/raw/pMTdQRb5'))() -- Atmosphere Script
-
         -- COPY THE ENTIRE MESSAGE OR CATVAPE WILL NOT WORK FOR YOU.
         getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
         loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
@@ -14,8 +12,6 @@ local function detectDevice()
         loadstring(game:HttpGet('https://gitlab.com/-/snippets/4774635/raw/main/BedwarsScript.lua'))() -- AlSploit
     else
         -- The player is on a PC
-        loadstring(game:HttpGet('https://pastebin.com/raw/pMTdQRb5'))() -- Atmosphere Script
-
         -- COPY THE ENTIRE MESSAGE OR CATVAPE WILL NOT WORK FOR YOU.
         getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
         loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
