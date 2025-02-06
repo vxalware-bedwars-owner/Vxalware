b686ab355b27fcf87c9d29ec92050f3d8417f06f
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-02-06 17:34:43 +0800

    Update Vxware-Bedwars.lua

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -1,25 +1,6 @@
-local UserInputService = game:GetService("UserInputService")
+-- Vxalware Rewrite
+getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
 
--- Function to detect device type
-local function detectDevice()
-    if UserInputService.TouchEnabled then
-        -- The player is on a mobile device
-        -- COPY THE ENTIRE MESSAGE OR CATVAPE WILL NOT WORK FOR YOU.
-        getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
-
-        loadstring(game:HttpGet('https://pastebin.com/raw/08HHpB92'))() -- Vxalware Extension
-        loadstring(game:HttpGet('https://gitlab.com/-/snippets/4774635/raw/main/BedwarsScript.lua'))() -- AlSploit
-    else
-        -- The player is on a PC
-        -- COPY THE ENTIRE MESSAGE OR CATVAPE WILL NOT WORK FOR YOU.
-        getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL" -- This is your key, please dont send it to anybody
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))() -- CatVape
-
-        loadstring(game:HttpGet('https://pastebin.com/raw/08HHpB92'))() -- Vxalware Extension
-        loadstring(game:HttpGet('https://pastebin.com/raw/gZhGD17A'))() -- Old AlSploit
-    end
-end
-
--- Run the detection when the script starts
-detectDevice()
+loadstring(game:HttpGet('https://pastebin.com/raw/08HHpB92'))() -- Vxalware Extension
+loadstring(game:HttpGet('https://gitlab.com/-/snippets/4774635/raw/main/BedwarsScript.lua'))() -- AlSploit
