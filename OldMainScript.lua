4b7a23eeed45d7b2bed78db19e4dfdcf0a9e42be
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-02-08 13:44:11 +0800

    Create OldMainScript.lua

---
+++ OldMainScript.lua
@@ -0,0 +1,17 @@
+local UserInputService = game:GetService("UserInputService")
+
+if UserInputService.TouchEnabled then
+    -- Vxalware Rewrite (mobile)
+    getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware%20Extension'))()
+    loadstring(game:HttpGet('https://gitlab.com/-/snippets/4774635/raw/main/BedwarsScript.lua'))()
+else
+    -- Vxalware Rewrite (PC)
+    getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware%20Extension'))()
+    loadstring(game:HttpGet('https://pastebin.com/raw/gZhGD17A'))()
+end
