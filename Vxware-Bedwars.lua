49e27c0ebc5eda3a7db83ad1ca62784044a2ed33
vxalware-bedwars-owner <jainasinth@gmail.com>
2025-02-15 11:27:01 +0800

    Update Vxware-Bedwars.lua

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -5,14 +5,14 @@ if UserInputService.TouchEnabled then
     getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware%20Extension'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware-Extension.lua'))()
     loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk", true))()
 else
     -- Vxalware Rewrite (PC)
     getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware%20Extension'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware-Extension.lua'))()
     loadstring(game:HttpGet("https://dpaste.com/CMGU6RSJW.txt",true))()
 end
 -- VXALWARE REWRITE REBORN COME BACK AFTER 1 WEEK OF DOWNTIME!
