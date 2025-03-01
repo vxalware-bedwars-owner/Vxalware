be7b4359b28a90c5b1744e08404f1ab34b1a2577
Vxalware <jainasinth@gmail.com>
2025-03-01 14:13:58 +0800

    Update Vxware-Bedwars.lua

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -52,11 +52,10 @@ if UserInputService.TouchEnabled then
     loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk", true))()
 else
     -- Vxalware Rewrite (PC)
-    getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware-Extension.lua"))()
-    loadstring(game:HttpGet("https://pastebin.com/raw/AZ4fEfUu",true))()
+    loadstring(game:HttpGet("https://pastebin.com/raw/g5azNFjf",true))() -- Extra Modules
 end
 
 -- Calculate execution time
