43cfd18d9efdefe690fbfd5e8e44daf374943214
Vxalware <jainasinth@gmail.com>
2025-03-10 21:24:16 +0800

    Vxalware Main Script Change

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -48,13 +48,13 @@ if UserInputService.TouchEnabled then
     getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware-Extension.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Assets/Vxalware%20Extension.lua"))()
     loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk", true))()
 else
     -- Vxalware Rewrite (PC)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Vxalware-Extension.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Assets/Vxalware%20Extension.lua"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/g5azNFjf",true))() -- Extra Modules
 end
 
