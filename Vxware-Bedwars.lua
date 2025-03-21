623ade667bd0f3d9835a40c8e87916cdd4ed3fba
Vxalware <jainasinth@gmail.com>
2025-03-21 10:59:07 +0800

    Update 4 CatV4

--- Vxware-Bedwars.lua
+++ Vxware-Bedwars.lua
@@ -45,8 +45,7 @@ local UserInputService = game:GetService("UserInputService")
 
 if UserInputService.TouchEnabled then
     -- Vxalware Rewrite (mobile)
-    getgenv().CAK = "CAK_KZ7DiDSB5-viNcjC-5AKdwKjP0EmqopL"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-is-back/CatV5/main/init.lua"))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/init.lua'))()
 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware-Rewrite/refs/heads/main/Assets/Vxalware%20Extension.lua"))()
     loadstring(game:HttpGet("https://alsploit.bedwars.workers.dev/MainScript.lua?Key=yyy7eG1DjWIulkrfnzDrFP2No1H3si8YVODj93SqCd0jFh1CVk", true))()
