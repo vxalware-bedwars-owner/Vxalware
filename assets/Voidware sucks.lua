e19df6a38c503334502249db5cfa4f46b054acf9
Vxalware <jainasinth@gmail.com>
2025-08-27 16:59:22 +0800

    Create Voidware sucks.lua

---
+++ assets/Voidware sucks.lua
@@ -0,0 +1,4 @@
+-- Loader UI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Loader%20UI.lua",true))()
+-- Rayfield UI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rayfield.lua",true))()
