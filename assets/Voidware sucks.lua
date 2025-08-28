0c33483b48ce2d63c313234a8add6b3bb693f416
Vxalware <jainasinth@gmail.com>
2025-08-28 16:03:25 +0800

    Delete assets directory

--- assets/Voidware sucks.lua
+++
@@ -1,4 +0,0 @@
--- Loader UI
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Loader%20UI.lua",true))()
--- Rayfield UI
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rayfield.lua",true))()
