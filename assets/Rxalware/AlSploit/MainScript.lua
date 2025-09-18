2e46a083062dbecded54b5b18524e12890823961
Vxalware <jainasinth@gmail.com>
2025-09-18 16:17:35 +0800

    Update MainScript.lua

--- assets/Rxalware/AlSploit/MainScript.lua
+++ assets/Rxalware/AlSploit/MainScript.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/0mBg0vYh", true))() -- AlSploit Config Loader
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/AlSploit%20ACL.lua", true))() -- AlSploit Config Loader
 task.wait(1.5)
 loadstring(game:HttpGet("https://pastebin.com/raw/dyJnaP0E", true))() -- AlSploit Modified
