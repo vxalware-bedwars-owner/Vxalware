8b42917303a518e943ea5e674e5253bff4e50883
Vxalware <jainasinth@gmail.com>
2025-09-20 21:23:45 +0800

    Update and rename notwatermark to notwatermark.lua

---
+++ assets/notwatermark.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0xEIite/rust/main/NewMainScript.lua"))()
+task.wait(4.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/Fix.lua"))()
