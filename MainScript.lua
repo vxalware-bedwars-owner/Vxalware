d004355e85c4f02c34d2b2a6ec206295e97cf8c2
Vxalware <jainasinth@gmail.com>
2025-09-21 11:19:56 +0800

    Tag is broken lol

--- MainScript.lua
+++ MainScript.lua
@@ -14,7 +14,7 @@ local Window = WindUI:CreateWindow({
 Window:Tag({
     Title = "v3.0.9",
     Color = Color3.fromHex("#30ff6a"),
-    Radius = 6.7,
+    Radius = 13,
 })
 
 Window:SetToggleKey(Enum.KeyCode.K)
