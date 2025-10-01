478011e08dc9cce8edde597382e748b2a9f5c244
Vxalware <jainasinth@gmail.com>
2025-10-01 19:26:44 +0800

    Fix loadstrings part 1 of upd 3.1.3

--- assets/notwatermark.lua
+++ assets/notwatermark.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0xEIite/rust/main/NewMainScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0xEIite/rust/main/NewMainScript.lua",true))()
 task.wait(4.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/Fix.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/Fix.lua",true))()
