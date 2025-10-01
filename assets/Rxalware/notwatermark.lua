a6fb3d1e08443281c91f4d7553902b4f5d2782f5
Vxalware <jainasinth@gmail.com>
2025-10-01 18:43:25 +0800

    Update notwatermark.lua

--- assets/Rxalware/notwatermark.lua
+++ assets/Rxalware/notwatermark.lua
@@ -1,8 +1,3 @@
---[[
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/MainScipt.lua"))()
-task.wait(4.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/Rust/MainScript.lua"))()
-]]
 if isfolder("rust") then
     delfolder("rust")
 end
