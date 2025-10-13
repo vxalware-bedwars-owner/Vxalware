2d44e90deb002f06a4225dd84863e19d3653dae9
Vxalware <jainasinth@gmail.com>
2025-10-13 22:10:52 +0800

    Cat Rewrite

--- assets/CatVape/CatV5/MainScript.lua
+++ assets/CatVape/CatV5/MainScript.lua
@@ -135,7 +135,7 @@ if device == "PC" then
 
 -- Mobile
 elseif device == "Mobile" then
--- pro code I think should work idrk 💀
+    -- pro code I think should work idrk 💀
     local profilePath = basePath .. "/profiles"
     makefolder(basePath)
     makefolder(profilePath)
@@ -147,27 +147,32 @@ elseif device == "Mobile" then
         "4777817887.gui.txt", "6035872082.gui.txt", "6079884123.gui.txt", "66654135.gui.txt",
         "6701277882.gui.txt", "7326934954.gui.txt", "7709344486.gui.txt", "7781392706.gui.txt",
         "7822444776.gui.txt", "80461030.gui.txt", "994732206.gui.txt",
+        "autowinn6872265039.txt",
         "billionaire client16008862571.txt", "billionaire client6872265039.txt",
         "billionaire client6872274481.txt", "billionaire v26872265039.txt",
-        "billionaire v26872274481.txt", "cc17625359962.txt", "closet client6872265039.txt",
-        "closet client6872274481.txt", "closet16008862571.txt", "closet6872265039.txt",
-        "closet6872274481.txt", "closet8444591321.txt", "color.txt", "commit.txt",
-        "default10449761463.txt", "default11630038968.txt", "default11979315221.txt",
-        "default121864768012064.txt", "default12355337193.txt", "default124596094333302.txt",
-        "default126509999114328.txt", "default13772394625.txt", "default137925884276740.txt",
-        "default142823291.txt", "default168556275.txt", "default17625359962.txt",
-        "default17750024818.txt", "default2788229376.txt", "default283721918.txt",
-        "default443406476.txt", "default4442272183.txt", "default4483381587.txt",
-        "default6872265039.txt", "default6872274481.txt", "default8768229691.txt",
-        "default893973440.txt", "default95004353881831.txt", "default96342491571673.txt",
-        "gui.txt", "hud.txt", "language.txt", "non.txt", "panic v26872274481.txt",
-        "panic6872265039.txt", "panic6872274481.txt", "pistonaire nut6872265039.txt",
-        "pistonaire nut6872274481.txt", "silent (only legit reach)6872265039.txt",
-        "silent (only legit reach)6872274481.txt", "spotify.txt",
-        "trillionaire config6872265039.txt", "trillionaire config6872274481.txt",
-        "whitelist.json"
+        "billionaire v26872274481.txt", "cc17625359962.txt",
+        "closet client6872265039.txt", "closet client6872274481.txt",
+        "closet16008862571.txt", "closet6872265039.txt",
+        "closet6872274481.txt", "closet8444591321.txt",
+        "color.txt", "commit.txt",
+        "default10449761463.txt", "default11630038968.txt",
+        "default11979315221.txt", "default121864768012064.txt",
+        "default12355337193.txt", "default124596094333302.txt",
+        "default126509999114328.txt", "default13772394625.txt",
+        "default137925884276740.txt", "default142823291.txt",
+        "default168556275.txt", "default17625359962.txt",
+        "default17750024818.txt", "default2788229376.txt",
+        "default283721918.txt", "default443406476.txt",
+        "default4442272183.txt", "default4483381587.txt",
+        "default6872265039.txt", "default6872274481.txt",
+        "default8768229691.txt", "default893973440.txt",
+        "default95004353881831.txt", "default96342491571673.txt",
+        "gui.txt", "hud.txt", "language.txt", "non.txt",
+        "panic6872265039.txt", "panic6872274481.txt",
+        "pistonaire nut6872265039.txt", "silent (only legit reach)6872265039.txt",
+        "spotify.txt", "trillionaire config6872265039.txt",
+        "trillionaire config6872274481.txt", "whitelist.json"
     }
-
 
     updateStatus("Loading profiles...")
     for _, fileName in ipairs(fileList) do
