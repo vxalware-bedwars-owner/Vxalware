c1fa95cd763c51e00d8592410e5b38a6f5cac9b2
Vxalware <jainasinth@gmail.com>
2025-10-20 12:27:15 +0800

    Update and rename notwatermark to CatVape.lua

---
+++ assets/CatVape/CatVape.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/init.lua'), 'init.lua')({
+    Username = "theholyjay",
+    Password = "H0s3vTSGQnKj",
+    Closet = false
+})
