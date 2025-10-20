1d3b029583bf23cd1eb03bfa8f566d88854f236a
Vxalware <jainasinth@gmail.com>
2025-10-20 17:20:57 +0800

    CatVape internal server error goes crazy ngl. I guess just have to wait for devs to fix issue

--- assets/CatVape/CatVape.lua
+++ assets/CatVape/CatVape.lua
@@ -1,5 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/init.lua'), 'init.lua')({
-    Username = "theholyjay",
-    Password = "H0s3vTSGQnKj",
-    Closet = false
-})
+loadstring(game:HttpGet('https://raw.githubusercontent.com/new-qwertyui/CatV5/main/init.lua'), 'init.lua')()
