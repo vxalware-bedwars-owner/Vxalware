6a3c8be193fd4a1b76bdca7b89185d8e801c8b92
Vxalware <jainasinth@gmail.com>
2025-12-25 11:45:28 +0530

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,6 @@
+--[[
+1. Download the specific scripting utility of your choice
+2. Execute the provided loadstring below ↓
+]]
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/MainScript.lua",true))()
