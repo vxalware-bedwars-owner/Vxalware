699ac46d8c161029378a3c9af175ad2528d00f15
SynthX <luminousray4@gmail.com>
2026-02-15 17:04:49 +0800

    Update and rename Rxalware.lua to Vxalware logo

--- assets/Rxalware.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0xEIite/rust/main/NewMainScript.lua",true))()
-task.wait(2)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxalware-bedwars-owner/Vxalware/refs/heads/main/assets/Rxalware/AlSploit/Fix.lua",true))()
